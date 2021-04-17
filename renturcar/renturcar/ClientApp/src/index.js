07c3a7271a98fa9bf207a0b5b535f8ae09da036b
Jorge <jorge.tamariz@aloe.com.do>
2021-04-16 20:32:11 -0400

    Add router on the hihger part of the app

--- renturcar/renturcar/ClientApp/src/index.js
+++ renturcar/renturcar/ClientApp/src/index.js
@@ -1,18 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import {setupAxios} from "./setupAxios";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
+
+setupAxios();
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>,
+    rootElement
+);
 
 registerServiceWorker();
-
