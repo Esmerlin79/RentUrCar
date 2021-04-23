bf92b9c14a0764e7a87e165a4743ea1c71474511
Jorge <jorge.tamariz@aloe.com.do>
2021-04-23 08:15:33 -0400

    Fix footer to the bottom

--- renturcar/renturcar/ClientApp/src/components/Footer.jsx
+++ renturcar/renturcar/ClientApp/src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div>
-            <footer className="col-md-12 bg-dark mt-8 p-10">
+        <>
+            <footer className=" fixed-bottom col-md-12 bg-dark mt-8 p-10">
                 <div className="container">
                     <p className="text-center text-white ">
                         &copy; RentUrCar | All rights reserved | {new Date().getFullYear()}
                     </p>
                 </div>
             </footer>
-        </div>
+        </>
     )
 }
